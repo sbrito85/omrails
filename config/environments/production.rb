b76f55fac856eb794677b99c95357fed415a6632
Steve Brito <sbrito85@gmail.com>
2013-03-23 00:27:19 -0700

    Add Devise user Model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,10 @@ Omrails::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  #required for devise on Heroku
+  config.assets.initialize_on_precompile = false
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
